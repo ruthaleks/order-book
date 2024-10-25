dd8c5dcbfb484a2e75d7e8a96d234be35973fa5b
Ruth Blankfors <ruth.aleks@gmail.com>
2024-10-25 22:25:09 +0200

    Spring boot template setup

---
+++ src/test/java/com/order_book/OrderBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
